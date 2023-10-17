fa46a9532b036fcb8bd82d37d4a8731361dee7b8
Goncalo Senra pg52683@alunos.uminho.pt <pg52683@search7edu2.local>
2023-10-17 19:39:26 +0100

    Original precision

--- code/src/MD.cpp
+++ code/src/MD.cpp
@@ -321,28 +321,28 @@ int main()
         Tavg += Temp;
         Pavg += Press;
 
-        fprintf(ofp, "  %8.4e  %20.8f  %20.8f %20.8f  %20.8f  %20.8f \n", i * dt * timefac, Temp, Press, KE, PE, KE + PE);
+        fprintf(ofp,"  %8.4e  %20.8f  %20.8f %20.8f  %20.8f  %20.8f \n",i*dt*timefac,Temp,Press,KE, PE, KE+PE);         
     }
-
+    
     // Because we have calculated the instantaneous temperature and pressure,
     // we can take the average over the whole simulation here
     Pavg /= NumTime;
     Tavg /= NumTime;
-    Z = Pavg * (Vol * VolFac) / (N * kBSI * Tavg);
-    gc = NA * Pavg * (Vol * VolFac) / (N * Tavg);
-    fprintf(afp, "  Total Time (s)      T (K)               P (Pa)      PV/nT (J/(mol K))         Z           V (m^3)              N\n");
-    fprintf(afp, " --------------   -----------        ---------------   --------------   ---------------   ------------   -----------\n");
-    fprintf(afp, "  %8.4e  %15.8f       %15.8f     %10.8f       %10.8f        %10.8e         %i\n", i * dt * timefac, Tavg, Pavg, gc, Z, Vol * VolFac, N);
-
-    printf("\n  TO ANIMATE YOUR SIMULATION, OPEN THE FILE \n  '%s' WITH VMD AFTER THE SIMULATION COMPLETES\n", tfn);
-    printf("\n  TO ANALYZE INSTANTANEOUS DATA ABOUT YOUR MOLECULE, OPEN THE FILE \n  '%s' WITH YOUR FAVORITE TEXT EDITOR OR IMPORT THE DATA INTO EXCEL\n", ofn);
-    printf("\n  THE FOLLOWING THERMODYNAMIC AVERAGES WILL BE COMPUTED AND WRITTEN TO THE FILE  \n  '%s':\n", afn);
-    printf("\n  AVERAGE TEMPERATURE (K):                 %15.8f\n", Tavg);
-    printf("\n  AVERAGE PRESSURE  (Pa):                  %15.8f\n", Pavg);
-    printf("\n  PV/nT (J * mol^-1 K^-1):                 %15.8f\n", gc);
-    printf("\n  PERCENT ERROR of pV/nT AND GAS CONSTANT: %15.8f\n", 100 * fabs(gc - 8.3144598) / 8.3144598);
-    printf("\n  THE COMPRESSIBILITY (unitless):          %15.8f \n", Z);
-    printf("\n  TOTAL VOLUME (m^3):                      %10.8e \n", Vol * VolFac);
+    Z = Pavg*(Vol*VolFac)/(N*kBSI*Tavg);
+    gc = NA*Pavg*(Vol*VolFac)/(N*Tavg);
+    fprintf(afp,"  Total Time (s)      T (K)               P (Pa)      PV/nT (J/(mol K))         Z           V (m^3)              N\n");
+    fprintf(afp," --------------   -----------        ---------------   --------------   ---------------   ------------   -----------\n");
+    fprintf(afp,"  %8.4e  %15.5f       %15.5f     %10.5f       %10.5f        %10.5e         %i\n",i*dt*timefac,Tavg,Pavg,gc,Z,Vol*VolFac,N);
+    
+    printf("\n  TO ANIMATE YOUR SIMULATION, OPEN THE FILE \n  '%s' WITH VMD AFTER THE SIMULATION COMPLETES\n",tfn);
+    printf("\n  TO ANALYZE INSTANTANEOUS DATA ABOUT YOUR MOLECULE, OPEN THE FILE \n  '%s' WITH YOUR FAVORITE TEXT EDITOR OR IMPORT THE DATA INTO EXCEL\n",ofn);
+    printf("\n  THE FOLLOWING THERMODYNAMIC AVERAGES WILL BE COMPUTED AND WRITTEN TO THE FILE  \n  '%s':\n",afn);
+    printf("\n  AVERAGE TEMPERATURE (K):                 %15.5f\n",Tavg);
+    printf("\n  AVERAGE PRESSURE  (Pa):                  %15.5f\n",Pavg);
+    printf("\n  PV/nT (J * mol^-1 K^-1):                 %15.5f\n",gc);
+    printf("\n  PERCENT ERROR of pV/nT AND GAS CONSTANT: %15.5f\n",100*fabs(gc-8.3144598)/8.3144598);
+    printf("\n  THE COMPRESSIBILITY (unitless):          %15.5f \n",Z);
+    printf("\n  TOTAL VOLUME (m^3):                      %10.5e \n",Vol*VolFac);
     printf("\n  NUMBER OF PARTICLES (unitless):          %i \n", N);
 
     fclose(tfp);
